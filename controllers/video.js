b2f5326bf217df5b7c457e321e41fa02709f6d28
aungzaw92 <sufyan99@gmail.com>
2023-09-28 11:44:47 -0700

    fix the address for having static images

--- controllers/video.js
+++ controllers/video.js
@@ -1,38 +1,40 @@
-const jsonService = require("../utils/jsonService");
-const { randomUUID } = require("crypto");
-const ip = require("ip");
+const fs = require('fs');
+const jsonService = require('../utils/jsonService');
+const { randomUUID } = require('crypto');
+const ip = require('ip');
 
 // Mock data for post requests
 const defaultVideoData = {
-  views: "1,001,023",
-  likes: "110,985",
-  duration: "5:01",
-  channel: "User Channel",
-  image: "Upload-video-preview.jpg",
+  views: '1,001,023',
+  likes: '110,985',
+  duration: '5:01',
+  channel: 'User Channel',
+  image: 'Upload-video-preview.jpg',
 };
 
 // Constants
-const DATABASE_PATH = "../data/videos.json";
+const DATABASE_PATH = '../data/videos.json';
+const data = jsonService.loadJSON(DATABASE_PATH);
 const BASE_URL = `http://${ip.address()}:${process.env.PORT}`;
 
-// Get videos
+//Get videos
 const getVideos = (req, res) => {
-  const data = jsonService.loadJSON(DATABASE_PATH);
   if (data) {
     let videos = data.map((video) => {
       const { id, title, channel, image } = video;
-      return { id, title, channel, image: `${BASE_URL}/images/${image}` };
+      return { id, title, channel, image: `${BASE_URL}/public/${image}` };
     });
+    console.log(videos);
     return res.status(200).json(videos);
   } else {
-    return res.status(500).json({ message: "Server error, no videos found." });
+    return res.status(500).json({ message: 'Server error, no videos found.' });
   }
 };
 
 // Get video
 const getVideo = (req, res) => {
   let { id } = req.params;
-  const data = jsonService.loadJSON(DATABASE_PATH);
+
   if (data && id) {
     let [video] = data.filter((video) => {
       return video.id === id;
@@ -40,7 +42,7 @@ const getVideo = (req, res) => {
     if (video) {
       return res
         .status(200)
-        .json({ ...video, image: `${BASE_URL}/images/${video.image}` });
+        .json({ ...video, image: `${BASE_URL}/public/${video.image}` });
     } else {
       return res.status(400).json({ message: `No video found for id: ${id}.` });
     }
@@ -60,7 +62,7 @@ const postVideo = (req, res) => {
   };
 
   // If some data is missing, return an error message
-  const requiredFields = ["title", "description"];
+  const requiredFields = ['title', 'description'];
   for (let field of requiredFields) {
     if (!newVideo[field]) {
       return res.status(400).json({ message: `Request body missing ${field}` });
--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const ip = require("ip");
-
+const ip = require('ip');
+console.log('HELLO!!');
+const path = require('path');
 // Middleware
-const corsMiddleware = require("./middleware/cors");
-const logger = require("./middleware/morgan");
+const corsMiddleware = require('./middleware/cors');
+const logger = require('./middleware/morgan');
 
 // Routers
-const registerRouter = require("./routes/register");
-const videoRouter = require("./routes/videos");
+const registerRouter = require('./routes/register');
+const videoRouter = require('./routes/videos');
 
 //Routes
 const PORT = process.env.PORT || 8080;
 
 app.use(corsMiddleware);
 app.use(express.json());
-app.use("/", logger);
+// app.use("/", logger);
 
 // Static images
-app.use(express.static("public"));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'));
 
 // Routers
-app.use("/register", registerRouter);
-app.use("/videos", videoRouter);
+app.use('/register', registerRouter);
+app.use('/videos', videoRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://${ip.address()}:${PORT}`);
